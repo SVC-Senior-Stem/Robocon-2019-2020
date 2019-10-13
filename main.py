f64ef398cf39b0d315a932161e023e21567ced47
16Dava <56504136+16Dava@users.noreply.github.com>
2019-10-13 12:02:05 +0100

    Created Base File For Code
    This is the base file for any and all code we write

---
+++ main.py
@@ -0,0 +1,3 @@
+# This is the base file for any and all code we write
+# There's nothing here yet, becuase I don't know whatwe want to code yet
+# In the first session of Senior STEM, we can write/plan the class architecture
