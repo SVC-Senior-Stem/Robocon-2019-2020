95f948ad72d5c4fcaff32de8da1a40196c67f173
16Dava <56504136+16Dava@users.noreply.github.com>
2019-11-06 16:07:00 +0000

    First proposal for the base architecture
    This is more as a log than an actual change
    Please use this architecture/style if you want to write our own code

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 # This is the base file for any and all code we write
 # There's nothing here yet, becuase I don't know whatwe want to code yet
 # In the first session of Senior STEM, we can write/plan the class architecture
+import robot
+
+class wheels:
+
+    @staticmethod
+    def drive_distance(distance):
+        pass
+    @staticmethod
+    def turn_angle(angle):
+        pass
+
+class conveyor:
+
+    @staticmethod
+    def release_cube():
+        pass
