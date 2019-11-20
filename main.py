7014819272d716afe79a7d7871e6b5b63751e741
16Dava <56504136+16Dava@users.noreply.github.com>
2019-11-20 15:37:22 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # This is the base file for any and all code we write
 # There's nothing here yet, becuase I don't know whatwe want to code yet
 # In the first session of Senior STEM, we can write/plan the class architecture
+# Test
 import robot
 
 class wheels:
